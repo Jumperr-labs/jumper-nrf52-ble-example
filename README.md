ce2030c16af11c0fa81306b40cc8a397c277d6b4
Dan Shemesh <dan@jumper.io>
2017-12-18 16:33:25 +0200

    working

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # jumper-nrf52-ble-example
+
+## Setup
+
+```bash
+sudo su
+cd /PATH/TO/jumper-nrf52-ble-examples/
+virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+
+### Usage
+
+- Open 3 different terminals, cd into jumper-nrf52-ble-examples directory and in all of them run:
+    ```bash
+    sudo su
+    cd /PATH/TO/jumper-nrf52-ble-examples/
+    source venv/bin/activate
+    ```
+- ***Terminal 1 -*** Start the virtual HCI device (BLE dongle): `jumper hci`
+- ***Terminal 2-*** Start the BLE gateway program: `python examples/ble_peripheral/ble_app_hrs/ble_gateway.py`
+- ***Terminal 3-*** Start the virtual nRF52 device with the ble_app_hrs example:
+    ```bash
+    cd cd examples/ble_peripheral/ble_app_hrs/jumper/
+    jumper run -b
+    jumper run -b ../pca10040/s132/armgcc/_build/nrf_and_softdevice.bin  
+    ```
+- Go back to ***terminal 2*** and follow the instructions
---
+++ examples/ble_peripheral/ble_app_hrs/ble_gateway.py
@@ -0,0 +1,62 @@
+from __future__ import print_function
+from builtins import input
+import struct
+from time import sleep
+import argparse
+import gattlib
+import pdb
+
+
+# def discover(device_id):
+#     discovery = gattlib.DiscoveryService(device_id)
+#     devices = discovery.discover(10)
+#
+#     for address, name in devices.items():
+#         print('Found device: {} {}'.format(name, address))
+#         # print("name: {}, address: {}".format(name, address))
+
+
+# class NrfRequester(gattlib.GATTRequester):
+#     CHARACTARISTIC_TO_NOTIFY = "00002a19-0000-1000-8000-00805f9b34fb"  # nrf52 measurement charactaristic
+#     # CHARACTARISTIC_TO_NOTIFY = "0000180f-0000-1000-8000-00805f9b34fb"  # nrf52 measurement charactaristic
+#     NOTIFY_ON   = struct.pack("<H", 0x0001)
+#     NOTIFY_OFF  = struct.pack("<H", 0x0000)
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-a', '--adapter-name', type=str, help='Adapter name', default='hci0')
+    parser.add_argument('-c', '--channel-type', type=str, help='Channel type', choices=('random', 'public'), default='random')
+    args = parser.parse_args()
+
+    print('Scanning for devices')
+    service = gattlib.DiscoveryService(args.adapter_name)
+    bd_addr = ''
+    device_name = ''
+
+    while bd_addr == '':
+        devices = service.discover(4)
+        for address, name in devices.items():
+            bd_addr = address
+            device_name = name
+
+    print('Found device: {} {}'.format(device_name, bd_addr))
+    sleep(1.5)
+    input('Press enter to connect...')
+    print('connecting to: {} {}'.format(device_name, bd_addr))
+    req = gattlib.GATTRequester(bd_addr, False, args.adapter_name)
+    req.connect(wait=True, channel_type=args.channel_type)
+    print('Connected')
+
+    try:
+        while True:
+            x = input('Press enter to read battery level (q to exist)...')
+            if x == 'q':
+                break
+            print('Battery level: {}'.format(ord(req.read_by_handle(0x16)[0])))
+
+    except KeyboardInterrupt:
+        pass
+
+
+if __name__ == '__main__':
+    main()
